f171d09083a591cf97b5fd6ca1aa23529e915f58
Aisel Sher <aisel.goo@gmail.com>
2023-10-30 16:30:58 +0300

    fix: change cover layout

--- components/shared/Cover.tsx
+++ components/shared/Cover.tsx
@@ -4,32 +4,28 @@ import Link from 'next/link';
 
 export function Cover() {
     return (
-        <article className="grid h-screen lg:grid-cols-2">
-            <section className="cover-column lg:bg-black">
-                <Image
-                    alt="r"
-                    src="/assets/melted-logo.png"
-                    width={560}
-                    height={0}
-                />
-            </section>
-            <section className="cover-column">
-                <div className="flex max-w-[400px] flex-col gap-10">
-                    <p className="text-center text-slogan-small md:text-slogan">
-                        KEEP YOUR HEAD{' '}
-                        <span className="cool" title="cool">
-                            COOL
-                        </span>
-                    </p>
-                    <Button asChild>
-                        <Link
-                            href="#about"
-                        >
-                            About
-                        </Link>
-                    </Button>
-                </div>
-            </section>
+        <article className="screen flex flex-col items-center justify-center">
+            <Image
+                alt="r"
+                src="/assets/melted-logo.png"
+                width={560}
+                height={0}
+            />
+            <div className="mt-32 flex max-w-[400px] flex-col gap-10">
+                <p className="text-center text-slogan-small md:text-slogan">
+                    KEEP YOUR HEAD{' '}
+                    <span className="cool" title="cool">
+                        COOL
+                    </span>
+                </p>
+                <Button asChild>
+                    <Link
+                        href="#about"
+                    >
+                        About
+                    </Link>
+                </Button>
+            </div>
         </article>
     );
 }
