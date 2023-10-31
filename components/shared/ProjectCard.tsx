27ff603811c70ec580ec82de9a1a578a506159a6
Aisel Sher <aisel.goo@gmail.com>
2023-10-31 16:37:11 +0300

    fix: project card mobile layout

--- components/shared/ProjectCard.tsx
+++ components/shared/ProjectCard.tsx
@@ -27,13 +27,10 @@ export function ProjectCard(props: ProjectCardProps) {
 
 
     return (
-        <div className={cn('relative grid grid-cols-8 gap-4')}>
+        <div className={cn('relative md:grid md:grid-cols-8 gap-4')}>
             <div className={cn(
                 `flex flex-col gap-4 rounded-md py-6
-                md:col-span-3
-                max-md:col-start-1 max-md:col-end-[-1]
-                max-md:row-start-1 max-md:row-end-[-1]
-                max-md:px-6 max-md:glass`,
+                md:col-span-3`,
                 reverse && 'md:text-right md:items-end'
             )}>
                 <h3 className="text-heading3-bold">
@@ -45,8 +42,17 @@ export function ProjectCard(props: ProjectCardProps) {
                         {title}
                     </Link>
                 </h3>
+                <div className="-mx-6 md:hidden">
+                    <Image
+                        className="object-cover shadow-lg"
+                        src={image}
+                        alt={title}
+                        width={1200}
+                        height={0}
+                    />
+                </div>
                 <div className={cn(
-                    'md:glass md:px-8 rounded-md py-6 text-body min-w-full',
+                    'md:glass md:px-8 rounded-md md:py-6 text-body min-w-full',
                     reverse ? 'md:-ml-40' : 'md:-mr-40'
                 )}>
                     {description}
@@ -86,10 +92,9 @@ export function ProjectCard(props: ProjectCardProps) {
             </div>
             <div
                 className={cn(
-                    `relative -z-10 overflow-hidden flex items-center
+                    `relative -z-10 flex items-center
                     md:col-span-5
-                    max-md:col-start-1 max-md:col-end-[-1]
-                    max-md:row-start-1 max-md:row-end-[-1]`,
+                    max-md:hidden`,
                     reverse && 'md:-order-1'
                 )}
             >
